cca397b36ab35954b06ae8ddd966d001812db325
AlekseyKhraptsov <FatalStar@mail.ru>
2022-07-26 00:22:38 +0500

    task16 done

---
+++ 16/task16.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	mass := []int{10, 18, 54, 65, 12, 36, 185, 8, 3, 92}
+	quickSort(mass, 0, len(mass)-1)
+	fmt.Println(mass)
+}
+
+func partition(arr []int, low, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
+}
+
+func quickSort(arr []int, low, high int) []int {
+	if low < high {
+		var p int
+		arr, p = partition(arr, low, high)
+		arr = quickSort(arr, low, p-1)
+		arr = quickSort(arr, p+1, high)
+	}
+	return arr
+}
