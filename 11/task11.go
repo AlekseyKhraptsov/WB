640a91d226bef6889bd79bfe63f6d82acf780e02
AlekseyKhraptsov <FatalStar@mail.ru>
2022-07-25 23:35:49 +0500

    task11 done

---
+++ 11/task11.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+/*
+Реализовать пересечение двух неупорядоченных множеств.
+*/
+
+func intersect(a, b []int) []int {
+	aSet := make(map[int]struct{})
+	bSet := make(map[int]struct{})
+
+	for _, x := range a {
+		aSet[x] = struct{}{}
+	}
+	for _, x := range b {
+		bSet[x] = struct{}{}
+	}
+	//Проверяем, есть ли ключ из первого во втором и если есть - выписываем его в out []int
+	var out []int
+	for x := range aSet {
+		_, ok := bSet[x]
+		if ok {
+			out = append(out, x)
+		}
+	}
+	return out
+}
+
+func main() {
+	fmt.Println(intersect([]int{1, 2, 3, 4}, []int{1, 2, 5, 6, 1}))
+}
